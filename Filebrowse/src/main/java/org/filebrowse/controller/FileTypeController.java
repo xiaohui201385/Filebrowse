81b541dff285add2db5bf55527391c578aabba02
zhiwei.Feng <15608507749@163.com>
2017-09-25 21:36:01 +0800

    update

--- Filebrowse/src/main/java/org/filebrowse/controller/FileTypeController.java
+++ Filebrowse/src/main/java/org/filebrowse/controller/FileTypeController.java
@@ -2,9 +2,11 @@ package org.filebrowse.controller;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.filebrowse.entity.FileType;
+import org.filebrowse.entity.PreviewFile;
 import org.filebrowse.service.FileTypeService;
 import org.filebrowse.service.PreviewFileService;
 import org.json.JSONArray;
@@ -37,13 +39,15 @@ public class FileTypeController {
     public List<FileType> getAfterAdd(@RequestParam(value="typeName",required=true)String typeName ){
         List<FileType> byName = fileTypeService.getByName(typeName);
         if(byName==null||byName.size()<1){
+            System.out.println(1);
             File file=new File("C:/Program Files/Microsoft Office Web Apps/OpenFromUrlWeb/docview/"+typeName+"/");
             file.mkdirs();
         }
         File[] typeList = getTypeList();
         previewFileService.delAll();
         fileTypeService.delAll();
-        initType(typeList);
+        List<FileType> initType = initType(typeList);
+        initFiles(initType);
         return fileTypeService.getAll();
     }
     
@@ -60,6 +64,26 @@ public class FileTypeController {
         }
         fileTypeService.addList(fileTypes);
         return fileTypes;
+    }
+    
+    private void initFiles(List<FileType> initTypes) {
+        List<PreviewFile> previewFiles=new ArrayList<>();
+        for(FileType fileType:initTypes){
+            int type=fileType.getId();
+            String typeName=fileType.getName();
+            File file=new File("C:/Program Files/Microsoft Office Web Apps/OpenFromUrlWeb/docview/"+typeName+"/");
+            File[] files = file.listFiles();
+            if(files.length>0){
+                for(File f:files){
+                    String fileName=f.getName();
+                    Date time=new Date(f.lastModified());
+                    String location=f.getAbsolutePath();
+                    PreviewFile previewFile = new PreviewFile(fileName, time, location, type);
+                    previewFiles.add(previewFile);
+                }
+            }
+        }
+        previewFileService.addList(previewFiles);
     }
     
     private File[] getTypeList(){
